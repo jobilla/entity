8460415e9aee5b90d7dcbf8f81b4534737a8d0ad
Hakan Aktas <iletisim@hakanaktas.com.tr>
2022-10-23 11:43:54 +0300

    export entity props types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Entity } from './Entity';
+export type { Props, PropsJson, PartialProps, PartialPropsJson } from './Entity';
 export { EntityBuilder } from './EntityBuilder';
 export { Type } from './support/Type';
