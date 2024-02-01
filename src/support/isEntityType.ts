af20518308c068b4afd256c81da1a543cb3f1411
Hakan Aktas <iletisim@hakanaktas.com.tr>
2024-02-01 19:57:16 +0200

    apply prettier to all files

--- src/support/isEntityType.ts
+++ src/support/isEntityType.ts
@@ -1,6 +1,8 @@
-import { Buildable, Constructor } from './Type';
-import { Entity } from '../Entity';
+import { Buildable, Constructor } from "./Type";
+import { Entity } from "../Entity";
 
-export function isEntityType(buildable: Buildable): buildable is Constructor<Entity> {
-    return buildable?.prototype instanceof Entity;
+export function isEntityType(
+  buildable: Buildable,
+): buildable is Constructor<Entity> {
+  return buildable?.prototype instanceof Entity;
 }
--- src/support/metadata/TypeMetadata.ts
+++ src/support/metadata/TypeMetadata.ts
@@ -1,53 +1,51 @@
 import {
-    PackedBuildable,
-    Buildable,
-    BuildableResolver,
-    Typeable,
-    Constructor,
-} from '../Type';
-import { Entity } from '../../Entity';
+  PackedBuildable,
+  Buildable,
+  BuildableResolver,
+  Typeable,
+  Constructor,
+} from "../Type";
+import { Entity } from "../../Entity";
 
 function isResolverFunction(type: Typeable): type is BuildableResolver {
-    // If the object's name is empty, we will assume it's an anonymous function that resolves the actual type.
-    return type.name?.length === 0;
+  // If the object's name is empty, we will assume it's an anonymous function that resolves the actual type.
+  return type.name?.length === 0;
 }
 
 export class TypeMetadata {
-    constructor(
-        public target: Constructor<Entity>,
-        public propertyName: string,
-        public sourcePropertyName: string,
-        private _type: Typeable
-    ) {}
+  constructor(
+    public target: Constructor<Entity>,
+    public propertyName: string,
+    public sourcePropertyName: string,
+    private _type: Typeable,
+  ) {}
 
-    public get type(): Buildable
-    {
-        if (isResolverFunction(this._type)) {
-            // Run the function to actually import the module and assign the module
-            // to type prop so that the EntityBuilder will actually get an entity
-            // constructor, and not a resolver function.
-            const resolvedType = (this._type)();
+  public get type(): Buildable {
+    if (isResolverFunction(this._type)) {
+      // Run the function to actually import the module and assign the module
+      // to type prop so that the EntityBuilder will actually get an entity
+      // constructor, and not a resolver function.
+      const resolvedType = this._type();
 
-            return TypeMetadata.unpackType(resolvedType);
-        }
-
-        return this._type;
+      return TypeMetadata.unpackType(resolvedType);
     }
 
-    private static unpackType(type: PackedBuildable): Buildable
-    {
-        // Assuming that deferred type is resolved via a 'require' function,
-        // if it is *not* appended by a key, like below...
-        // @Type( () => require('./foo') )
-        // It will resolve into an object, and since we are no magicians here,
-        // this will simply return the default exported item. If the entity
-        // class is not exported as default, the 'require' must have a key
-        // appended to it like below:
-        // @Type( () => require('./foo').Foo )
-        if (typeof type === 'object') {
-            return type.default;
-        }
+    return this._type;
+  }
 
-        return type;
+  private static unpackType(type: PackedBuildable): Buildable {
+    // Assuming that deferred type is resolved via a 'require' function,
+    // if it is *not* appended by a key, like below...
+    // @Type( () => require('./foo') )
+    // It will resolve into an object, and since we are no magicians here,
+    // this will simply return the default exported item. If the entity
+    // class is not exported as default, the 'require' must have a key
+    // appended to it like below:
+    // @Type( () => require('./foo').Foo )
+    if (typeof type === "object") {
+      return type.default;
     }
+
+    return type;
+  }
 }
